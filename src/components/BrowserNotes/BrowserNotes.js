7052ed4b7d6fd478aac574209439f97dc5358826
Dhruv Shah <dhruv.shah4192@gmail.com>
2019-04-20 15:37:49 +0200

    updated browser bug

--- src/components/BrowserNotes/BrowserNotes.js
+++ src/components/BrowserNotes/BrowserNotes.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import Browser from '@components/Browser';
+import React from "react";
+import Browser from "@components/Browser";
 
 const BrowserNotes = () => {
   return (
     <div className="browser-notes">
-      <Browser except firefox chrome mobile>
-        <span>All the tricks in this site work best in <b>Chrome!</b></span>
+      <Browser only firefox chrome mobile>
+        <span>
+          All the tricks in this site work best in <b>Chrome!</b>
+        </span>
       </Browser>
       <Browser only firefox>
-        <span>Application Background is disabled in FireFox. Try in <b>Chrome!</b></span>
+        <span>
+          Application Background is disabled in FireFox. Try in <b>Chrome!</b>
+        </span>
       </Browser>
     </div>
   );
