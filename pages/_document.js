2c94c84ed85f745a640ffa673d341bf3a0e20029
Dhruv Shah <code4192@gmail.com>
2021-08-25 09:56:59 +0200

    fix: added ganalytics script

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,19 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `,
+          }} 
+        />
           <script
             dangerouslySetInnerHTML={{
               __html: `(function() {
